e980409c88e89c60f0b4b1f177f0b97161b08304
boosterlong <chartrandblake93@gmail.com>
2022-12-20 15:43:40 -0800

    Changing icon to personal icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,8 @@ module.exports = {
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
-        display: `minimal-ui`
+        display: `minimal-ui`,
+        icon: `src/images/blake-icon.png`, // This path is relative to the root of the site.
       },
     },
   ],
